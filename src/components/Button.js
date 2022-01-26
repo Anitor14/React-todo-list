f78ba690d70c03b393c9a81728312f968e7ee148
Anitor <anitorabraham@gmail.com>
2022-01-26 01:42:26 +0100

    choreUpdates() User Interface completed

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+const Button = ({ color, text, onClick }) => {
+  //   const onClick = (e) => {
+  //     // console.log(e);
+  //   };
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.prototype = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+};
+export default Button;
